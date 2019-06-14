da21fdf6fccf8b5ee549a3ae65878887a97ad3fc
omar <omasrt@gmail.com>
2019-06-14 15:38:48 -0500

    add main app and auth

---
+++ snackstalker/main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+# from django.http import 
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def posts_index(request):
+  posts = Post.objects.all()
+  return render(request, 'posts/index.html', {'posts': posts})
